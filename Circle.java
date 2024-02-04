5e01bdc6b81cb243aa4ca0d829afcbd873550391
TediiSyah <156158722+TediiSyah@users.noreply.github.com>
2024-02-04 15:22:05 +0700

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.contohabstract;
+
+/**
+ *
+ * @author Tedii
+ */
+    public class Circle extends Shape {
+
+    private float radius;
+
+    public Circle(float radius) {
+        this.radius = radius;
+    }
+    
+    
+    @Override
+    float getArea() {
+        return (float) (Math.PI * radius * radius);
+    }
+    
+}
+
